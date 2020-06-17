9047c55603940acf801f356809125635af6783e2
Brian Lego <legobw@gmail.com>
2020-06-17 00:47:10 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,6 @@
 # Write your solution here
-this_year=2019
-puts "Hey, it's not 2019!" 
-unless this_year == 2019
+
+puts "You know what year it is??"
+this_year = 2019
+puts "Hey, it's 2019!" if this_year==2019
+puts "Hey, it's not 2019!" unless this_year == 2019
