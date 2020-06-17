95091b035148f9f9e1714b604e184a527de5260f
Brian Lego <legobw@gmail.com>
2020-06-17 00:47:28 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -3,4 +3,4 @@
 puts "You know what year it is??"
 this_year = 2019
 puts "Hey, it's 2019!" if this_year==2019
-puts "Hey, it's not 2019!" unless this_year == 2019
+puts "Hey, it's not 2019!" unless this_year == 2014
