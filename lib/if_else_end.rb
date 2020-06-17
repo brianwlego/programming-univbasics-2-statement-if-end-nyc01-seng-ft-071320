babff25679b00215d266636fe69d95ba2a19f6c5
Brian Lego <legobw@gmail.com>
2020-06-17 00:53:30 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,7 @@
 # Write your solution here
 
-puts "You know what year it is??"
-this_year = 2014
-puts "Hey, it's 2019!" if this_year==2019
-puts "Hey, it's not 2019!" unless this_year == 2019
+current_time = Time.now
+if (current_time.to_i == ev)
+  puts "Even!"
+else
+  puts "Odd!"
